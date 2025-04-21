4f05ff97eef036b752fa63a4507bc306c776efa4
user <victorap.punk@gmai,com>
2025-04-21 14:19:17 +0000

    login

--- app/src/main/java/com/example/traveltdea/MainActivity.java
+++ app/src/main/java/com/example/traveltdea/MainActivity.java
@@ -12,20 +12,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnStartNavigation;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnStartNavigation = findViewById(R.id.btnStartNavigation);
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
 
-        btnStartNavigation.setOnClickListener(new View.OnClickListener() {
+
+                btnStartNavigation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                // Aquí puedes agregar la lógica para validar el correo electrónico y la contraseña
+                // y hacer una llamada a la API si es necesario.
+
                 Intent intent = new Intent(MainActivity.this, MapActivity.class);
                 startActivity(intent);
             }
