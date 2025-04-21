641ba35b3fa44e11e32a88609c5804617bf4b50e
user <victorap.punk@gmai,com>
2025-04-21 16:53:40 +0000

    object

---
+++ app/src/main/java/com/example/traveltdea/User.java
@@ -0,0 +1,28 @@
+package com.example.traveltdea;
+
+public class UserModel {
+    private String email;
+    private String username;
+    private String password;
+    private String createdAt;
+    private String lastLogin;
+    private DeviceInfo deviceInfo;
+
+    public UserModel() { } // Constructor vacío para Firebase
+
+    public UserModel(String email, String username, String password, String createdAt, String lastLogin, DeviceInfo deviceInfo) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.createdAt = createdAt;
+        this.lastLogin = lastLogin;
+        this.deviceInfo = deviceInfo;
+    }
+
+    public String getEmail() { return email; }
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public String getCreatedAt() { return createdAt; }
+    public String getLastLogin() { return lastLogin; }
+    public DeviceInfo getDeviceInfo() { return deviceInfo; }
+}
